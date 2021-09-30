7c389d7fbb1e6d34cda8d987e24cb83409a8d239
AleksandraMotor <aleksandramotor@gmail.com>
2021-09-30 23:52:11 +0200

    added new icons

--- script.js
+++ script.js
@@ -22,13 +22,33 @@ icon4.addEventListener('click', function () {
 });
 
 const icon6 = document.querySelector('.burger6');
+const bars6 = document.querySelector('.bars6');
+const times6 = document.querySelector('.times6');
 icon6.addEventListener('click', function () {
     icon6.classList.toggle('active6');
+    bars6.classList.toggle('opacity6');
+    times6.classList.toggle('opacity6');
 });
 
 const icon8 = document.querySelector('.burger8');
 const meat = document.querySelector('.meat');
 icon8.addEventListener('click', function () {
     meat.textContent === 'menu' ? meat.textContent = 'close' : meat.textContent = 'menu';
+});
+
+const icon9 = document.querySelector('.burger9');
+icon9.addEventListener('click', function () {
+    icon9.classList.toggle('active9'); 
+});
+
+const icon10 = document.querySelector('.burger10');
+const text10 = document.querySelector('.text10');
+const bars10 = document.querySelector('.bars10');
+const times10 = document.querySelector('.times10');
+icon10.addEventListener('click', function () {
+    icon10.classList.toggle('active10');
+    text10.classList.toggle('active10');
+    bars10.classList.toggle('opacity10');
+    times10.classList.toggle('opacity10');
 });
 // div.style.transform = "rotate(" + rotate + "deg)";
